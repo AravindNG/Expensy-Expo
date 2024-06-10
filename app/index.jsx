f9ce3f3da222cd86b4d4f5b06fa4559f5b94cb82
AravindNG <aravindnaik296@gmail.com>
2024-06-10 22:30:48 +0530

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,52 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, Text, View, ScrollView } from 'react-native';
+import { Redirect, router } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
+
+export default function App() {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{height : '100%'}}>
+        <View className=" w-full justify-center items-center px-4 ">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          
+          <View className="relative mt-5">
+            <Text className="text-2xl text-white font-bold font text-center">
+              Discover Expense Expenditure{"\n"}
+              with{"\n"}{" "}
+              <Text className="text-secondary-200 text-3xl">Expensy</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[138px] h-[16px] absolute-bottom -right-24"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where we can save something precious, somehow</Text>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push('/sign-in')}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
+  );
+}
